bfa6087f9b96bee3e42ae828912f24ba18ae239f
Leandro Ostera <me@leostera.com>
2013-10-13 10:27:14 -0300

    fix(require): Adds RoutesProvider to index.js
    chore(component.json): Adds description.
    Bumps version.
    Adds keywords.
    
    chore(comment): Removes comment from routes.js
    We actually can't afford losing that specific route.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var app = angular.module('ng2Auth', ['ngRoute']);
 
 require('./providers/oauth2');
+require('./providers/routes');
 require('./services/interceptors');
 require('./controllers/auth-callback');
 require('./config.js');
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 angular.module('ng2Auth')
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    // Feel free to remove this root route :)
     .when('/auth/:provider/callback', {
       controller: 'authCallback',
       template: require('./views/auth-callback')
